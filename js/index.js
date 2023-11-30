2dfc05d098abff8118ee16eaf911d5a701f482ee
lucas-pettinelli <pettinellilucas@gmail.com>
2023-11-30 15:08:01 -0300

    primeiro commit do projeto

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const horE1 = document.getElementById("hora");
+const minE1 = document.getElementById("minuto");
+const segE1 = document.getElementById("segundo");
+
+function atualizacaoRelogio() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    horE1.innerText = h;
+    minE1.innerText = m;
+    segE1.innerText = s;
+
+    setTimeout(() => {
+        atualizacaoRelogio()
+    }, 1000)
+
+}
+
+function mudarBackgroundDiaOuNoite(){
+    let horaAtual = new Date().getHours();
+    if(5 < horaAtual && horaAtual < 18){
+        if (document.body) {
+            document.body.style.backgroundImage = 'linear-gradient(to top, #56ccf2, #2f80ed)';
+        }
+    } else {
+        if (document.body) {
+            document.body.style.backgroundImage = 'linear-gradient(to bottom, #141e30, #243b55)'
+        }
+    }
+}   
+
+
+atualizacaoRelogio()
+mudarBackgroundDiaOuNoite()
